8279647c18a7eaec4ceaa2f3bb27481a4c31c52d
wonjongin <wonggamggik@gmail.com>
2024-02-14 14:36:35 +0900

    JS-MusicControl

--- README.md
+++ README.md
@@ -12,32 +12,32 @@
 
 <h2> 게임 클래스 및 함수 </h2>
 
-- 🟪게임 플레이 버튼 관리 - class onGame
+- 🟩게임 플레이 버튼 관리 - class onGame
 
-  - 🟪게임 시작 버튼을 누를 시 게임 시작 - onStart()
-  - 🟪게임 정지 버튼을 누를 시 게임 종료 - onStop()
+  - 🟩게임 시작 버튼을 누를 시 게임 시작 - onStart()
+  - 🟩게임 정지 버튼을 누를 시 게임 종료 - onStop()
     > 게임 시작 버튼 클릭 이후에 실행<br>
     > 모두 초기화(onStart() 이전 상태로 전환)
 
-- 🟪게임 결과 - class resultGame
+- 🟩게임 결과 - class resultGame
 
-  - 🟪게임 승리 - winGame()
-  - 🟪게임 패배 - loseGame()
+  - 🟩게임 승리 - winGame()
+  - 🟩게임 패배 - loseGame()
 
-- 🟪게임의 정보 - class ingameInform : carrotCount, bugCount, time
+- 🟩게임의 정보 - class ingameInform : carrotCount, bugCount, time
 
   1. carrotCount: 당근의 개수 지정함
   2. bugCount: 벌레의 개수를 지정함
   3. time: 제한 시간을 지정함
 
-  - 🟪당근이 정해진 공간안에 생성 - makeCarrot()
+  - 🟩당근이 정해진 공간안에 생성 - makeCarrot()
 
     > 게임 시작 버튼 클릭 이후에 실행
 
-    - 🟪당근 선택 시 당근의 개수 view 1개씩 감소<br>
+    - 🟩당근 선택 시 당근의 개수 view 1개씩 감소<br>
     - (+)당근 터치 시 음악 실행 playMusic()#playPickCarrot()
 
-  - 🟪벌레가 정해진 공간안에 생성 - makeBug()
+  - 🟩벌레가 정해진 공간안에 생성 - makeBug()
 
     > 게임 시작 버튼 클릭 이후에 실행<br>
 
@@ -45,24 +45,24 @@
     - (+)벌레 터치 시 음악 실행 playMusic()#playPickBug()<br>
     - (+)벌레 터치 음악 종료 후 패배 음악 실행 playMusic()#playLoseMusic()
 
-  - 🟪당근의 개수가 나옴 - viewCarrotCount()
+  - 🟩당근의 개수가 나옴 - viewCarrotCount()
 
     > 게임 시작 버튼 클릭 이후에 실행
 
     - (+)0이 되면 게임 승리 resultGame()#winGame()
     - (+)0이 되면 음악 실행 playMusic()#playWinMusic()
 
-  - 🟪타이머 시작 - startTimer()
+  - 🟩타이머 시작 - startTimer()
     > 게임 시작 버튼 클릭 이후에 실행
     - (+)0이 되면 게임 패배 resultGame()#loseGame()
     - (+)0이 되면 음악 실행 playMusic()#playLoseMusic()
 
-- 🟪음악 실행 - class musicCotrol : audio
+- ✅음악 실행 - class musicCotrol : audio
 
 1. audio: 어느 audio 객체가 들어갈 것인지 지정
 
-- 🟪게임 진행 음악 실행 - playMusic()
-- 🟪게임 패배 시 음악 실행 - stopMusic()
+- ✅게임 진행 음악 실행 - playMusic()
+- ✅게임 패배 시 음악 실행 - stopMusic()
 
 <h2> 반성할 점 </h2>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@
 
 // class resultGame {}
 
-// class ingameInform {}
+class ingameInform {
+  constructor(carrotCount, bugCount, time) {
+    this.carrotCount = carrotCount;
+    this.bugCount = bugCount;
+    this.time = time;
+  }
+}
 
 class musicCotrol {
   constructor(audio) {
@@ -20,20 +26,26 @@ class musicCotrol {
 const gameButton = document.querySelector(".button__start");
 const gameButtonIcon = document.querySelector(".button__start > i");
 
-// audio 변수들
+// audio 변수
 const audio__bg = document.querySelector(".audio__bg");
 const audio__alert = document.querySelector(".audio__alert");
 const audio__bug = document.querySelector(".audio__bug");
 const audio__carrot = document.querySelector(".audio__carrot");
 const audio__win = document.querySelector(".audio__win");
-const audioBg = new musicCotrol(audio__bg);
-const audioAlert = new musicCotrol(audio__alert);
-const audioBug = new musicCotrol(audio__bug);
-const audioCarrot = new musicCotrol(audio__carrot);
-const audioWin = new musicCotrol(audio__win);
 
 gameButton.addEventListener("click", (e) => {
-  if (audio__bg.paused) {
+  playBGM(audio__bg);
+});
+
+// Timer
+const timer = document.querySelector(".game__timer");
+const timerText = document.querySelector(".game__timer-text");
+
+// function
+// 게임이 시작할 때 BGM을 실행시키는 함수
+function playBGM(sound) {
+  const audioBg = new musicCotrol(sound);
+  if (sound.paused) {
     audioBg.playMusic();
     gameButtonIcon.classList.remove("fa-play");
     gameButtonIcon.classList.add("fa-stop");
@@ -42,7 +54,15 @@ gameButton.addEventListener("click", (e) => {
     gameButtonIcon.classList.remove("fa-stop");
     gameButtonIcon.classList.add("fa-play");
   }
-});
+}
 
-// const music = new playMusic();
-// music.playBGM();
+// BGM을 제외한 이외의 효과음들
+function playSoundEffect(sound) {
+  const soundEffect = new musicCotrol(sound);
+  if (sound.paused) {
+    soundEffect.playMusic();
+  } else {
+    soundEffect.stopMusic();
+    soundEffect.playMusic();
+  }
+}
